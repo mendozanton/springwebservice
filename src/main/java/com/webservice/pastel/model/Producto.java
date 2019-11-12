45a5a41e2446ca661eca702348b412ea5e8822f1
abraham <abrmendoza32@gmail.com>
2019-11-12 15:09:51 -0800

    actualizado producto.estado de numerico a caracter 30

--- src/main/java/com/webservice/pastel/model/Producto.java
+++ src/main/java/com/webservice/pastel/model/Producto.java
@@ -16,131 +16,112 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.webservice.pastel.model.Ingrediente;
-
 @Entity
 @Table(name = "PRODUCTO")
-public class Producto implements Serializable{
-	
+public class Producto implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idProducto;
-	
-	
+
 	@Column(length = 30, nullable = false)
 	private String nombre;
-	
+
 	@Column(length = 100)
 	private String descripcion;
-	
+
 	@Column(nullable = false)
 	private Double precio;
-	
+
 	@Column(nullable = false)
 	private Integer stock;
-	
-	@Column(length = 25,columnDefinition = "NUMERIC")
+
+	@Column(length = 30)
 	private String estado;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "idPromocion", foreignKey = @ForeignKey(name="FK_Promocion_Producto"))
+	@JoinColumn(name = "idPromocion", foreignKey = @ForeignKey(name = "FK_Promocion_Producto"))
 	private Promocion promocion;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "producto")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "producto")
 	private Set<Ingrediente> ingredientes;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "producto")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "producto")
 	private Set<Pan> panes;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "producto")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "producto")
 	private Set<Torta> torta;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "producto")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "producto")
 	private Set<Postre> postre;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "producto")
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "producto")
 	private Set<Compra> compras;
-	
+
 	public Producto() {
 		// TODO Auto-generated constructor stub
 	}
-
 
 	public Long getIdProducto() {
 		return idProducto;
 	}
 
-
 	public void setIdProducto(Long idProducto) {
 		this.idProducto = idProducto;
 	}
-
 
 	public Promocion getPromocion() {
 		return promocion;
 	}
 
-
 	public void setPromocion(Promocion promocion) {
 		this.promocion = promocion;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 
 	public Double getPrecio() {
 		return precio;
 	}
 
-
 	public void setPrecio(Double precio) {
 		this.precio = precio;
 	}
-
 
 	public Integer getStock() {
 		return stock;
 	}
 
-
 	public void setStock(Integer stock) {
 		this.stock = stock;
 	}
-
 
 	public String getEstado() {
 		return estado;
 	}
 
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
 
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
 
 	@Override
 	public String toString() {
